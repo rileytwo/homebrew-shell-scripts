124e303b529a4f2c5c5912dea0fbef57ebad56aa
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-05-04 18:21:18 -0500

    Pass brew audit tests

--- Formula/color-codes-sh.rb
+++ Formula/color-codes-sh.rb
@@ -1,10 +1,10 @@
 class ColorCodesSh < Formula
-    desc "Print ANSI color codes"
-    homepage "https://github/rileytwo/shell-scripts"
-    url "https://github/rileytwo/shell-scripts.git"
-    version "0.1.0"
+  desc "Print ANSI color codes"
+  homepage "https://github/rileytwo/shell-scripts"
+  url "https://github/rileytwo/shell-scripts.git"
+  version "0.1.0"
 
-    def install
-      bin.install "color-codes.sh"
-    end
+  def install
+    bin.install "color-codes.sh"
   end
+end
--- Formula/zshup-sh.rb
+++ Formula/zshup-sh.rb
@@ -1,10 +1,10 @@
 class BiSh < Formula
-    desc "Upgrade Oh-My-Zsh! custom plugins"
-    homepage "https://github/rileytwo/shell-scripts"
-    url "https://github/rileytwo/shell-scripts.git"
-    version "0.1.0"
+  desc "Upgrade Oh-My-Zsh! custom plugins"
+  homepage "https://github/rileytwo/shell-scripts"
+  url "https://github/rileytwo/shell-scripts.git"
+  version "0.1.0"
 
-    def install
-      bin.install "zshup.sh"
-    end
+  def install
+    bin.install "zshup.sh"
   end
+end
