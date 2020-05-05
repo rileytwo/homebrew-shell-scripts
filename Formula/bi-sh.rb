59760d9cd088210c520a84eb3a989ea849f8898b
rileytwo <34988536+rileytwo@users.noreply.github.com>
2020-05-05 18:57:38 -0500

    update to 0.2.0

--- Formula/bi-sh.rb
+++ Formula/bi-sh.rb
@@ -2,7 +2,7 @@ class BiSh < Formula
   desc "Find the CFBundleIdentifier of an application"
   homepage "https://github.com/rileytwo/shell-scripts"
   url "https://github.com/rileytwo/shell-scripts.git"
-  version "0.1.0"
+  version "0.2.0"
 
   def install
     bin.install "bi.sh"
