67acf53307b7e1ed53452178230312a80e836489
rileytwo <34988536+rileytwo@users.noreply.github.com>
2021-09-18 12:04:09 -0500

    update to v0.5.0

--- Formula/zshup-sh.rb
+++ Formula/zshup-sh.rb
@@ -2,7 +2,7 @@ class ZshupSh < Formula
   desc "Upgrade Oh-My-Zsh! custom plugins"
   homepage "https://github.com/rileytwo/shell-scripts"
   url "https://github.com/rileytwo/shell-scripts.git", :branch => 'main'
-  version "0.4.0"
+  version "0.5.0"
 
   def install
     bin.install "bin/zshup.sh"
